7c81c8a56f944bb5e0c1f948e49222a97a50e87c
oswaldo <oswaldoadidas@hotmail.com>
2019-01-13 15:25:34 -0600

    a

---
+++ src/enseñanza/Enseñanza.java
@@ -0,0 +1,43 @@
+
+package enseñanza;
+
+
+public class Enseñanza {
+
+    
+    public static void main(String[] args) {
+        
+        int numero  = 200; 
+        int numero1 = 500;
+        int numero2 = 500;
+        //int resultado = numero*numero1*numero2;
+        
+        if(numero2 < numero){
+            System.out.println("numero2 es mayor que numero");           
+        }else if(numero2 >= numero1){
+            System.out.println("numero2 es igual a numero1");
+           
+        }else if(numero1 != numero){
+            System.out.println("me imprimo");
+        }else{
+            System.out.println("puto EPn");
+        }
+        
+        
+        //System.out.println(numero3+" "+numero);
+        
+        
+        /*System.out.println(numero);
+        System.out.println(cadena);
+        System.out.println(booleano);
+        System.out.println(boleano2);
+        */
+        
+        // + - * / %  OPERADORES MATEMATICOS
+        // < > <= >= == != OPERADORES DE COMPARACIONES
+        
+   
+    
+    }
+    
+}
